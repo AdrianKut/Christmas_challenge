552422748be980a6b6e4305413733608d2093aab
AdrianKut <adrian.kut96@onet.pl>
2022-12-09 12:33:20 +0100

    Replace old arraylsit in day9

--- ChristmasChallenge/Day9_VowelsCount.cs
+++ ChristmasChallenge/Day9_VowelsCount.cs
@@ -8,20 +8,60 @@ namespace ChristmasChallenge
      * Pomóż Mikołajowi i policz ile będzie musiał dopłacić, a policzoną wartość wypisz na ekranie.
      * Adresy znajdziesz w przygotowanej liście, nie ma w nich polskich znaków.
      */
+
     internal class Day9_VowelsCount : IShowResult
     {
+        private static List<Address> PrepareAddresses()
+        {
+            List<Address> addresses = new List<Address>();
+
+            addresses.Add(new Address("Serniczkowa",
+                    "4B",
+                    2,
+                    "Krakow",
+                    "02-326"
+            ));
+            addresses.Add(new Address("Pierniczkowa",
+                    "289A",
+                    55,
+                    "Gdansk",
+                    "02-326"
+            ));
+            addresses.Add(new Address("Barszczykowa",
+                    "234",
+                    5,
+                    "Rzeszow",
+                    "37-326"
+            ));
+            addresses.Add(new Address("Uszkowa",
+                    "43H",
+                    5,
+                    "Wroclaw",
+                    "02-326"
+            ));
+            addresses.Add(new Address("Cukierkowa",
+                    "23I",
+                    5,
+                    "Poznan",
+                    "02-326"
+            ));
+
+            return addresses;
+        }
+
         public void ShowResult()
         {
-            List<Address> addresses = Address.PrepareAddresses()
-                .Distinct(new DistinctAddressesComparer()).ToList();
-
-            Regex regexVowels = new Regex("[aeiouy]", RegexOptions.IgnoreCase);
+            Regex regexVowels = new Regex("[eayiou]", RegexOptions.IgnoreCase);
             int vowelsCounter = 0;
             int extraPay = 2;
 
-            addresses.ForEach(x => vowelsCounter 
+            List<Address> addresses = PrepareAddresses();
+
+            addresses.ForEach(x => vowelsCounter
             += regexVowels.Matches(x.Street).Count()
-            + regexVowels.Matches(x.City).Count());
+            + regexVowels.Matches(x.City).Count()
+            + regexVowels.Matches(x.HouseNumber).Count()
+            );
 
             Console.Write($"\nExtra payment: {(vowelsCounter * extraPay)}");
         }
